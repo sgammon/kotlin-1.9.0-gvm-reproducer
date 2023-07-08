bf41f8875b882b39f78753ac55e8300c03275f8a
Sam Gammon <sam@elide.ventures>
2023-07-08 16:23:29 -0700

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,76 @@
+import com.google.devtools.ksp.gradle.KspTaskJvm
+
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "1.9.0"
+    id("org.jetbrains.kotlin.plugin.allopen") version "1.9.0"
+    id("com.google.devtools.ksp") version "1.9.0-1.0.11"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("io.micronaut.application") version "4.0.0-M8"
+}
+
+version = "0.1"
+group = "com.example"
+
+val kotlinVersion=project.properties.get("kotlinVersion")
+
+repositories {
+    maven("https://s01.oss.sonatype.org/content/repositories/snapshots/") {
+        mavenContent { snapshotsOnly() }
+    }
+    mavenCentral()
+}
+
+dependencies {
+    ksp("info.picocli:picocli-codegen")
+    ksp("io.micronaut.serde:micronaut-serde-processor")
+    implementation("info.picocli:picocli")
+    implementation("io.micronaut.kotlin:micronaut-kotlin-runtime")
+    implementation("io.micronaut.picocli:micronaut-picocli")
+    implementation("io.micronaut.serde:micronaut-serde-jackson")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:${kotlinVersion}")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:${kotlinVersion}")
+    compileOnly("org.graalvm.nativeimage:svm")
+    runtimeOnly("ch.qos.logback:logback-classic")
+    runtimeOnly("com.fasterxml.jackson.module:jackson-module-kotlin")
+}
+
+
+application {
+    mainClass.set("com.example.DemoCommand")
+}
+
+java {
+    sourceCompatibility = JavaVersion.toVersion("19")
+}
+
+tasks {
+    compileKotlin {
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_19)
+        }
+    }
+    compileTestKotlin {
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_19)
+        }
+    }
+}
+
+micronaut {
+    testRuntime("junit5")
+    processing {
+        incremental(true)
+        annotations("com.example.*")
+    }
+}
+
+afterEvaluate {
+  tasks.named<KspTaskJvm>("kspKotlin").configure {
+    kotlinOptions {
+      apiVersion = "1.9"
+      languageVersion = "1.9"
+      jvmTarget = "19"
+    }
+  }
+}
+
---
+++ src/main/kotlin/com/example/DemoCommand.kt
@@ -0,0 +1,30 @@
+package com.example
+
+import io.micronaut.configuration.picocli.PicocliRunner
+import io.micronaut.context.ApplicationContext
+
+import picocli.CommandLine
+import picocli.CommandLine.Command
+import picocli.CommandLine.Option
+import picocli.CommandLine.Parameters
+
+@Command(name = "demo", description = ["..."],
+        mixinStandardHelpOptions = true)
+class DemoCommand : Runnable {
+
+    @Option(names = ["-v", "--verbose"], description = ["..."])
+    private var verbose : Boolean = false
+
+    override fun run() {
+        // business logic here
+        if (verbose) {
+            println("Hi!")
+        }
+    }
+
+    companion object {
+        @JvmStatic fun main(args: Array<String>) {
+            PicocliRunner.run(DemoCommand::class.java, *args)
+        }
+    }
+}
